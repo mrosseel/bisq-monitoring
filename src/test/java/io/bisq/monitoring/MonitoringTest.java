04f2add6d03576b76dae755158d33468431ac0c5
Mike Rosseel <mike@eon-consult.be>
2017-12-13 16:18:42 +0100

    Fix regex for getting the fee

--- src/test/java/io/bisq/monitoring/MonitoringTest.java
+++ src/test/java/io/bisq/monitoring/MonitoringTest.java
@@ -1,7 +1,33 @@
 package io.bisq.monitoring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.fail;
+
 /*
 
- */public class MonitoringTest {
+ */
+@Slf4j
+public class MonitoringTest {
+
+    @Test
+    public void regexTest() {
+        String testSTring = "{ \"dataMap\": { \"dogeTxFee\": 5000000, \"dashTxFee\": 50, \"btcTxFee\": 380, \"ltcTxFee\": 500 }, \"bitcoinFeesTs\": 1513173628 }";
+        //"btcTxFee": 310
+        Pattern p = Pattern.compile("\"btcTxFee\":\\s*(\\d+),");
+        Matcher m = p.matcher(testSTring);
+        log.info(m.toString());
+        if(m.find()) {
+            log.info(m.group(1));
+        } else {
+           log.error("Can't find txfee");
+           fail();
+        }
+
+    }
 
 }
